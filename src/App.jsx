2443f7143c8392f8d49116d9c1ba23936ae77fba
realemma4 <realemma4@gmail.com>
2025-12-06 13:30:43 +0100

    edit index.html

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 import About from "./components/About";
 import WhyOurCompany from "./components/WhyOurCompany";
 import Services from "./components/Services";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 
